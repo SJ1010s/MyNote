98d30c8c83213d163680a64e9c8ebf803ef59490
Evgeny <71824125+SJ1010s@users.noreply.github.com>
2021-02-18 16:24:00 +0700

    Initial commit

---
+++ app/src/main/java/ru/home/mynote/NoteStructure.java
@@ -0,0 +1,14 @@
+package ru.home.mynote;
+
+public class NoteStructure {
+    private String title;
+    private String descr;
+    private String date;
+
+    public NoteStructure(String title, String descr, String date) {
+        this.title = title;
+        this.descr = descr;
+        this.date = date;
+    }
+}
+
